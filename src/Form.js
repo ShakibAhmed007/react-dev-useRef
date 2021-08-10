b75b67bd32c3f9156bd9956fa9e9068f956f4653
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-10 17:25:05 +0600

    1 file modified

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const Form = () => {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    // component did load
+    inputRef.current.focus();
+  }, []);
   return (
     <div>
-      <input type="text" placeholder="enter something" />
+      <input ref={inputRef} type="text" placeholder="enter something" />
+      <input type="text" placeholder="enter something 1" />
+      <input type="text" placeholder="enter something 2" />
     </div>
   );
 };
