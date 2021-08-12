71e49d53556a1d0f3c102f84e7b413c16dfab20b
ShakibAhmed007 <shakibahmed09112017@gmail.com>
2021-08-12 10:34:15 -0700

    1 file modified

--- src/Input.js
+++ src/Input.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// if component is wrapped using forwardref then ref will passed  by react
 const Input = (props, ref) => {
   const { type, placeholder } = props;
   return <input ref={ref} type={type} placeholder={placeholder} />;
